1e86491c5bcb2a19eb24d83b7dee6e09ac3d8e8b
Kirin-KL <klevakin-Kirilll@yandex.ru>
2026-05-22 14:31:47 +0500

    Поддержка домена

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    allowedHosts: ['rus.kk-vui.online'], // ← добавляем это
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
